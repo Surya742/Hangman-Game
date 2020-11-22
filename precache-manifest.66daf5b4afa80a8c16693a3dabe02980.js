d0e7d5eb6367573c76c8591771f1407f83ec1176
Surya742 <suryakumarsahani.742@gmail.com>
2020-11-22 06:16:00 +0530

    Updates

---
+++ precache-manifest.66daf5b4afa80a8c16693a3dabe02980.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a286d2f42d30602047a258fbf7b54a0",
+    "url": "/hangman-game/index.html"
+  },
+  {
+    "revision": "1f5370be1ad75c9fba0a",
+    "url": "/hangman-game/static/css/main.6be78a49.chunk.css"
+  },
+  {
+    "revision": "065a46daa5a9e0d8ad66",
+    "url": "/hangman-game/static/js/2.f61e9be8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-game/static/js/2.f61e9be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5370be1ad75c9fba0a",
+    "url": "/hangman-game/static/js/main.0c62f23f.chunk.js"
+  },
+  {
+    "revision": "da5386d2f10469872ed7",
+    "url": "/hangman-game/static/js/runtime-main.ef57f811.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/hangman-game/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/hangman-game/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/hangman-game/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/hangman-game/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/hangman-game/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/hangman-game/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/hangman-game/static/media/6.c80394c2.jpg"
+  }
+]);
